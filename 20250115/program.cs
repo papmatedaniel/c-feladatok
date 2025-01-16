3713b1529d5b85c42be71c610be4f8e0cb5c70ae
papmatedaniel <90318564+papmatedaniel@users.noreply.github.com>
2025-01-16 18:30:21 +0100

    Update program.cs

--- 20250115/program.cs
+++ 20250115/program.cs
@@ -23,12 +23,14 @@ namespace _20250116
 	}
 	class Program
 	{
+		static Dictionary<int, int> statok = new Dictionary<int, int> { {1,0}, { 2, 0 }, { 3, 0 }, { 4, 0 }, { 5, 0 }, { 6, 0 } };
 		static List<Helsinki> adatok = new List<Helsinki>();
 		static void Main(string[] args)
 		{
 			beolvasas();
 			f3();
 			f4();
+			f5();
 			f6();
 			f7();
 			f8();
@@ -43,12 +45,12 @@ namespace _20250116
 				adatok.Add(new Helsinki(item));
 			}
 		}
-		static void f1()
+		static void f3()
 		{
 			Console.WriteLine("3. feladat: ");
-			Console.WriteLine($"pontszerző helyezések száma: {adatok.Count}");
+			Console.WriteLine($"Pontszerző helyezések száma: {adatok.Count}");
 		}
-		static void f3()
+		static void f4()
 		{
 			Dictionary<int, int> statisztika = new Dictionary<int, int>();
 			Console.WriteLine("4. feladat: ");
@@ -65,23 +67,18 @@ namespace _20250116
 			}
 			foreach (var item in statisztika)
 			{
-				switch (item.Key)
-				{
-					case 1:
-						Console.WriteLine($"Arany: {item.Value}");
-						break;
-					case 2:
-						Console.WriteLine($"Ezüst: {item.Value}");
-						break;
-					case 3:
-						Console.WriteLine($"Bronz: {item.Value}");
-						break;
-				}
+					statok[item.Key] += item.Value;
 			}
+            Console.WriteLine($"Arany: {statok[1]}");
+			Console.WriteLine($"Ezüst: {statok[2]}");
+			Console.WriteLine($"Bronz: {statok[3]}");
+            Console.WriteLine($"Összesen: {statok[1] + statok[2] + statok[3]}");
+
 		}
-		static void f4()
+		static void f5()
 		{
-			/*érthetetlen feladat*/
+			Console.WriteLine("5. feladat");
+			Console.WriteLine($"Olimpiai pontok száma: {statok[1] * 7 + statok[2] * 5 + statok[3] * 4 + statok[4] * 3 + statok[5] * 2 + statok[6]}");
 		}
 		static void f6()
 		{
@@ -113,7 +110,6 @@ namespace _20250116
 		}
 		static void f7()
 		{
-			//javítani!!!
 			string filenev = "helsinki2.txt";
 			using (FileStream fs = File.Create(filenev)) { }
 			foreach (var item in adatok)
@@ -136,6 +132,12 @@ namespace _20250116
 		{
 			Console.WriteLine("8. feladat");
 			int legnagyobb = adatok.Max(item => item.sportoloKszama);
+			int indexe = adatok.FindIndex(item => item.sportoloKszama == legnagyobb);
+			Console.WriteLine($"Helyezés: {adatok[indexe].helyezes}");
+			Console.WriteLine($"Sportág: {adatok[indexe].sportag}");
+			Console.WriteLine($"Versenyszám: {adatok[indexe].versenyszam}");
+			Console.WriteLine($"Sportolók száma: {legnagyobb}");
+			/*
 			foreach (var item in adatok)
 			{
 				if (item.sportoloKszama == legnagyobb)
@@ -147,6 +149,7 @@ namespace _20250116
 					break;
 				}
 			}
+			*/
 		}
 	}
 }
